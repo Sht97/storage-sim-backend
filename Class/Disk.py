dc1e9b0df63bc22226efcf8720ee1cac26e694da
Daniel Rivera <Danielf.r97@gmail.com>
2021-01-12 13:22:17 -0500

    validations and round

--- Class/Disk.py
+++ Class/Disk.py
@@ -2,14 +2,14 @@ from Schemes.Disk import DiskBase
 
 class Disk:
     def __init__(self,base:DiskBase):
-        self.Trotation=(60000/base.rpm)/2
+        self.Trotation=round((60000/base.rpm)/2,2)
         self.Tseek=base.average_seek
 
-        self.random={"Ttransfer":4000/(1024*base.max_transfer) }
+        self.random={"Ttransfer":round(4000/(1024*base.max_transfer),4)}
         self.random['Tio']=self.Trotation+self.Tseek+self.random['Ttransfer']
-        self.random['Rio']=4/self.random['Tio']
+        self.random['Rio']=round(4/self.random['Tio'],4)
 
 
-        self.secuential={"Ttransfer":100000/base.max_transfer}
-        self.secuential['Tio']=self.Trotation+self.Tseek+self.secuential['Ttransfer']
-        self.secuential['Rio']=100*1024/self.secuential['Tio']
+        self.secuential={"Ttransfer":round(100000/base.max_transfer,4)}
+        self.secuential['Tio']=round(self.Trotation+self.Tseek+self.secuential['Ttransfer'],4)
+        self.secuential['Rio']=round(100*1024/self.secuential['Tio'],3)
--- Schemes/Disk.py
+++ Schemes/Disk.py
@@ -1,11 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional
 
 
 class DiskBase(BaseModel):
     name: str
     capacity: float
-    rpm: float
-    average_seek: float
-    max_transfer: float
+    rpm:float= Field(..., gt=0, description="The rpm must be greater than zero")
+    average_seek: float= Field(..., gt=0, description="The average seek must be greater than zero")
+    max_transfer: float= Field(..., gt=0, description="The max transfer must be greater than zero")
     platters: int
     cache: int
+
